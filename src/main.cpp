f26b05f3f0079696482218eaf840e8c76755db25
Giacomo Manna <giacomo.manna@gmail.com>
2021-08-04 11:20:25 +0200

    Implemented moisture sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include "esp_wps.h"
+#include <PubSubClient.h>
 
 
 #define ESP_WPS_MODE      WPS_TYPE_PBC
@@ -8,9 +9,14 @@
 #define ESP_MODEL_NAME    "ESPRESSIF IOT"
 #define ESP_DEVICE_NAME   "ESP STATION"
 
-static esp_wps_config_t config;
-RTC_DATA_ATTR bool wps_success = false;
+const char* mqtt_server = "broker.hivemq.com";
+const char* mqtt_topic = "binoz/esp32/moisture";
+const unsigned int mqtt_port = 1883;
 
+const int LED_PIN = 14;
+int blink_counter = 0;
+
+static esp_wps_config_t config;
 
 void wps_init_config(){
   config.wps_type = ESP_WPS_MODE;
@@ -20,6 +26,7 @@ void wps_init_config(){
   strcpy(config.factory_info.model_name, ESP_MODEL_NAME);
   strcpy(config.factory_info.device_name, ESP_DEVICE_NAME);
 }
+
 
 void wps_start(){
     if ( esp_wifi_wps_enable(&config)){
@@ -35,59 +42,39 @@ void wps_stop(){
     }
 }
 
-void print_wakeup_reason(){
-  esp_sleep_wakeup_cause_t wakeup_reason;
-
-  wakeup_reason = esp_sleep_get_wakeup_cause();
-
-  switch(wakeup_reason)
-  {
-    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case ESP_SLEEP_WAKEUP_TIMER : 
-      Serial.println("Wakeup caused by timer"); 
-    break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
-    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
-  }
-}
-
 bool is_wifi_connected(){
   return WiFi.status() == WL_CONNECTED;
 }
 
-void handle_wifi_event(WiFiEvent_t event){
+void log_wifi_event(WiFiEvent_t event){
   switch(event){
     
     case SYSTEM_EVENT_STA_CONNECTED:
       Serial.println("WiFi connected");
      
     break;
-
     case SYSTEM_EVENT_STA_GOT_IP:
       Serial.println("Got IP address: ");
-      Serial.println(WiFi.localIP());
+    break;
+
+     case SYSTEM_EVENT_STA_LOST_IP:
+      Serial.println("Lost IP address: ");
     break;
 
     case SYSTEM_EVENT_STA_DISCONNECTED:
        Serial.println("WiFi disconnected!");
+       WiFi.reconnect();
     break;
 
-    case SYSTEM_EVENT_STA_WPS_ER_SUCCESS: /**< ESP32 station wps succeeds in enrollee mode */
-      Serial.println("WPS Connection successfull");
-      wps_success = true;
-      wps_stop();
-      delay(10);
-      WiFi.begin();
-      Serial.println(WiFi.status());
+    case SYSTEM_EVENT_STA_WPS_ER_SUCCESS: 
+       Serial.println("WPS Successfull, stopping WPS and connecting to: " + String(WiFi.SSID()));
     break;       
     
-    case SYSTEM_EVENT_STA_WPS_ER_FAILED:        /**< ESP32 station wps fails in enrollee mode */
+    case SYSTEM_EVENT_STA_WPS_ER_FAILED:        
       Serial.println("WPS Connection failed!");
     break;
     
-    case SYSTEM_EVENT_STA_WPS_ER_TIMEOUT:       /**< ESP32 station wps timeout in enrollee mode */
+    case SYSTEM_EVENT_STA_WPS_ER_TIMEOUT:   
       Serial.println("WPS Connection timeout!");
     break;
     
@@ -96,54 +83,101 @@ void handle_wifi_event(WiFiEvent_t event){
   }
 
 }
+void light_led(int duration){
+  digitalWrite(LED_PIN,HIGH);
+  delay(duration);
+  digitalWrite(LED_PIN,LOW);
+}
 
-void setup_wifi(){
-  WiFi.mode(WIFI_MODE_STA);
-  WiFi.onEvent(handle_wifi_event);
-
-  if (!is_wifi_connected()){
-    Serial.println("WiFi is not connected.");
-    if (!wps_success){
-       wps_init_config();
-       wps_start();
-    }else{
-      Serial.println("Reconnecting");
-      WiFi.reconnect();
-    }
+void led_blink(){
    
-  }
+   blink_counter++;
+
+
+   if (WiFi.status() != WL_CONNECTED){
+     if (blink_counter % 2 == 0){
+       digitalWrite(LED_PIN,HIGH);
+     }else{
+       digitalWrite(LED_PIN,LOW);
+     }
+     delay(1000);  
+   }else{
+     digitalWrite(LED_PIN,LOW);
+   }
+   
 }
 
-
-void setup_sleep(){
-  esp_sleep_enable_timer_wakeup(5000000);
+void on_wps_connected(WiFiEvent_t event){
+    Serial.println("Connecting to WiFi");
+    wps_stop();
+    delay(10);
+    WiFi.begin();
 }
 
+void on_wifi_connected(WiFiEvent_t event){
+  
+  WiFiClient wifi_client;
+  PubSubClient mqtt_client(wifi_client);
+  
+  mqtt_client.setServer(mqtt_server, mqtt_port);  
+  mqtt_client.connect("ESP32MoistureSensor");
+  
+  Serial.println("Sending mqtt message");
 
+  int moisture = analogRead(39);
+  char moisture_str[10]; 
+  String(moisture).toCharArray(moisture_str,10);
+  Serial.println(moisture);
+  Serial.println(moisture_str);
+  mqtt_client.publish(mqtt_topic,moisture_str);
+  light_led(2000);
 
-void loop(){
- 
+  Serial.println("Entering Sleep");
+  esp_deep_sleep_start();
+  Serial.println("Exiting Sleep");
+
 }
 
 void setup()
 {
     Serial.begin(115200);
+    pinMode(LED_PIN,OUTPUT);
 
-    //print_wakeup_reason();
+    esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
     
-    setup_wifi();
-    setup_sleep();
-    
-    
-    while (!is_wifi_connected()){
-       Serial.println("waiting for wifi to connect...");
-       delay(1000);
+    WiFi.mode(WIFI_MODE_STA);
+    WiFi.onEvent(log_wifi_event);
+    WiFi.onEvent(on_wps_connected,SYSTEM_EVENT_STA_WPS_ER_SUCCESS);
+    WiFi.onEvent(on_wifi_connected,SYSTEM_EVENT_STA_GOT_IP);
 
-    }
-
-    Serial.println("Entering Sleep");
-    esp_deep_sleep_start();
-    Serial.println("Exiting Sleep");
+    if (!is_wifi_connected()){
+      
+      Serial.println("WiFi is not connected.");
+      
+      if (wakeup_reason == ESP_SLEEP_WAKEUP_TIMER){
+      
+        Serial.println("Waking up from sleep, reconnetting wo WiFi using previous configuration");
+        WiFi.begin();
+      
+      }else{
+        Serial.println("Starting WPS");
+        wps_init_config();
+        wps_start();
+      
+      }
     
+    }    
+      
+    esp_sleep_enable_timer_wakeup(5000000);
+    
+
+    
+}
+
+
+
+void loop(){
+  led_blink();
+
 }
 
